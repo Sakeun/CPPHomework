4a08835b75e996169bf38fff2a07c1e7c1ff0203
Sietse <sietsevanderzee@gmail.com>
2024-04-03 17:19:50 +0200

    add bit printing

--- BitOperators/BitOperators.cpp
+++ BitOperators/BitOperators.cpp
@@ -5,6 +5,15 @@
 #include "Field.h"
 #include "Unit.h"
 
+// Example to print bits, just in case :)
+void printChar(char c, int len) {
+    const char mask = 0b1;
+    for (int i = len; i >= 0; i--) {
+        std::cout << ((c >> i) & mask);
+    }
+    std::cout << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
     // Exercise 1:
@@ -25,6 +34,16 @@ int main(int argc, char* argv[])
     char* story = new char[1024]("hottentottententententoonstelling");
     cipher.de_cipher(story, '%');
     cipher.de_cipher(story, new char[10]("abcdefg"), 7);
+
+    // Printing chars:
+    unsigned char* bits = new unsigned char[]
+    {
+        0b0011010, 0b1100110, 0b1110101
+    };
+    std::cout << "Printing chars example: " << std::endl;
+    printChar(bits[0], 7);
+    printChar(bits[1], 7);
+    printChar(bits[2], 7);
 
     return 0;
 }
